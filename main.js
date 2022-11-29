1b066dde2c95b9e7409d94981bda525692d68e55
Bannan8 <117390552+Bannan8@users.noreply.github.com>
2022-11-29 03:03:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('.menu__burger-btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+
+    // $('body').on('click', function () {
+    //     $('.menu__list--active').toggleClass('menu__list')
+    // });
+});
